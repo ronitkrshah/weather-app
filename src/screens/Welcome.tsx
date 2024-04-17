f9e0c5713fd35d2c884885231c1fd4c85008da97
RKS <118371892+ronitkrshah@users.noreply.github.com>
2024-04-18 00:08:08 +0530

    Get navigation as prop in welcome screen
    We can access navigation as prop without invoking useNavigation function

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -2,18 +2,19 @@ import { StyleSheet, View } from "react-native";
 import { ActivityIndicator, Text } from "react-native-paper";
 import { BaseLayout } from "../layouts/Base";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { useNavigation } from "@react-navigation/native";
 import publicIP from "react-native-public-ip";
-import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import type { StackNavigationRoutes } from "src/types/navigation/routes/stack";
 import { useStore } from "src/store";
 import { weatherApi } from "src/api/weatherApi";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { FC } from "react";
 
-export const WelcomeScreen = () => {
+// Navigation Props
+type Props = NativeStackScreenProps<StackNavigationRoutes, "Welcome">;
+
+export const WelcomeScreen: FC<Props> = ({ navigation }) => {
   const setWeatherData = useStore((state) => state.setWeatherData);
-  const navigation =
-    useNavigation<NativeStackNavigationProp<StackNavigationRoutes>>();
 
   /*
    * Since Welcome screen only show on stratup there's no need to use useEffect
