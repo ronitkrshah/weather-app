bab0ce1ceb0c287cd037933ddb74f7656f7e8fe2
RKS <118371892+ronitkrshah@users.noreply.github.com>
2024-04-18 01:49:59 +0530

    Move Header to shared components directory & Add show date option

---
+++ src/components/Shared/Header/index.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import { StyleSheet, View } from "react-native";
+import { Text, useTheme } from "react-native-paper";
+import { useStore } from "src/store";
+
+type Props = {
+  showDate?: boolean;
+};
+
+export const Header: FC<Props> = ({ showDate = true }) => {
+  const weatherData = useStore((state) => state.weatherData);
+
+  const { colors } = useTheme();
+
+  // Render
+  return (
+    <View>
+      <Text variant="titleLarge" style={{ textAlign: "center" }}>
+        {weatherData &&
+          `${weatherData.location.name}, ${weatherData.location.country}`}
+      </Text>
+
+      {/* Date */}
+      {showDate && (
+        <Text style={{ ...styles.date, backgroundColor: colors.primary }}>
+          {new Date().toDateString()}
+        </Text>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  date: {
+    marginTop: 10,
+    textAlign: "center",
+    color: "#FFF",
+    width: "50%",
+    alignSelf: "center",
+    borderRadius: 20,
+    elevation: 2,
+    fontSize: 16,
+    transform: [{ translateY: -5 }],
+  },
+});
