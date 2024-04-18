2fe798a4194cb5555f6340fa61b1756a545b18a8
RKS <118371892+ronitkrshah@users.noreply.github.com>
2024-04-18 23:22:22 +0530

    Add temperature in Forecast Screen

--- src/components/Screen/Forecast/Card.tsx
+++ src/components/Screen/Forecast/Card.tsx
@@ -10,32 +10,56 @@ export const Card: FC<{ data: Forecastday }> = ({ data }) => {
         ...styles.container,
       }}
     >
-      <CardItem
-        icon="air"
-        title={`${data?.day.maxwind_kph} Km/h`}
-        subtitle="WindSpeed"
-      />
-      <CardItem
-        icon="water-drop"
-        title={`${data?.day.avghumidity}%`}
-        subtitle="Humidity"
-      />
-      <CardItem
-        icon="visibility"
-        title={`${data?.day.avgvis_km} Km`}
-        subtitle="Visibility"
-      />
+      <View style={styles.innerContainer}>
+        <CardItem
+          icon="thermometer-low"
+          title={`${data?.day.mintemp_c}°`}
+          subtitle="Min Temperature"
+        />
+        <CardItem
+          icon="thermometer"
+          title={`${data?.day.avgtemp_c}°`}
+          subtitle="Avg Temperature"
+        />
+        <CardItem
+          icon="thermometer-high"
+          title={`${data?.day.maxtemp_c}°`}
+          subtitle="Max Temperature"
+        />
+      </View>
+      <View style={styles.innerContainer}>
+        <CardItem
+          icon="weather-windy"
+          title={`${data?.day.maxwind_kph} Km/h`}
+          subtitle="Avg WindSpeed"
+        />
+        <CardItem
+          icon="water-outline"
+          title={`${data?.day.avghumidity}%`}
+          subtitle="Avg Humidity"
+        />
+        <CardItem
+          icon="eye"
+          title={`${data?.day.avgvis_km} Km`}
+          subtitle="Avg Visibility"
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-evenly",
     marginVertical: 10,
     paddingVertical: 10,
     borderRadius: 20,
+    flexWrap: "wrap",
+    gap: 25,
+  },
+  innerContainer: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
   },
 });
