4aa9d0fbb047497477fba6f511aae069fadc0e26
RKS <118371892+ronitkrshah@users.noreply.github.com>
2024-04-19 13:40:06 +0530

    Show Today & Tomorrow in Forecast Screen Instead of just week day

--- src/utils/getDayFromDate.ts
+++ src/utils/getDayFromDate.ts
@@ -1,4 +1,21 @@
 export default function getDayFromDate(date: string) {
+  // Current Date (Today)
+  const currDate = new Date();
+
+  // Date from Param
   const newDate = new Date(date);
+
+  // Return Today
+  if (currDate.getDate() == newDate.getDate()) {
+    return "Today";
+  }
+
+  currDate.setDate(currDate.getDate() + 1);
+
+  // Return Tomorrow
+  if (currDate.getDate() == newDate.getDate()) {
+    return "Tomorrow";
+  }
+
   return newDate.toLocaleDateString("en-US", { weekday: "long" });
 }
